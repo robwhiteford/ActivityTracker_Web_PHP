9c55449aa16e7b88aba5fe8874cb673ba317e8e3
Rob-Whiteford <44391482+Rob-Whiteford@users.noreply.github.com>
2020-08-30 09:43:03 +0100

    creates database connection

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+    $host = "localhost";
+
+    $user = "root";
+
+    $pass = "";
+
+    $database = "robwhzru_stats";
+
+    $connection = mysqli_connect($host,$user,$pass,$database) or die("Could not connect to database!");
+
+?>
